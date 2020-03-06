a9e28743042c3715b66e1fd679af5af6f59cca47
agus24 <agusx244@gmail.com>
2020-03-06 21:29:20 +0700

    initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+const mix = require('laravel-mix')
+
+// setup
+mix.disableSuccessNotifications();
+
+// email receipt
+mix.sass("./scss/receipt_email/index.scss", "build/receipt_email/receipt_email.css")
