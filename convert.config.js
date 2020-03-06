d36731c232f6c93cf79b031630d9920a15a072d2
agus24 <agusx244@gmail.com>
2020-03-07 00:59:35 +0700

    auto inliner

---
+++ convert.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    file: "/templates/receipt_email.html",
+    css: "/build/receipt_email/receipt_email.css",
+    result_file: "",  // default result.html
+}
--- purge.js
+++ purge.js
@@ -1,9 +1,12 @@
 const PurgeCSS = require("purgecss").PurgeCSS
+const Inliner = require("html-inline")
 const fs = require("fs")
+const config = require("./convert.config.js")
+const result = config.result_file === "" ? "result.html" : config.result_file
 
-const purged = new PurgeCSS().purge({
-    content: ['./templates/receipt_email.html'],
-    css: ["./build/receipt_email/receipt_email.css"]
+new PurgeCSS().purge({
+    content: [__dirname + config.file],
+    css: [__dirname + config.css]
 }).then(response => {
     let result = response[0]
     fs.writeFile(result.file, result.css, err => {
@@ -13,3 +16,12 @@ const purged = new PurgeCSS().purge({
         console.log("success write file")
     })
 })
+
+let inliner = Inliner({
+    ignoreImages: true,
+    ignoreLinks: true,
+    ignoreScripts: true,
+})
+
+let output = fs.createWriteStream(__dirname + "/converted/" + result)
+fs.createReadStream(__dirname + config.file).pipe(inliner).pipe(output)
